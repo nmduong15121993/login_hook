03b62ae520031bc74b6f7f6334c849853882333e
dũng nguyễn <dungnguyen@dungs-Mac.local>
2021-03-04 22:57:41 +0700

    add random state

--- src/mooks/apis/constant.js
+++ src/mooks/apis/constant.js
@@ -1,2 +1,17 @@
 // Thời gian lấy data
-export const TIME_OUT_FETCH = 2000;
+export const TIME_OUT_FETCH = (() => {
+  // time
+  const randomTimeOut = [1500, 2500, 3500, 1000, 500];
+
+  // get index random
+  const getIndRandom = (arrRandom) => {
+    const indRandom = Math.floor(Math.random() * arrRandom.length);
+    return indRandom;
+  }
+  // status
+  const status = [false, true];
+  return {
+    time: randomTimeOut[getIndRandom(randomTimeOut)],
+    status: status[getIndRandom(status)],
+  };
+})();
--- src/mooks/apis/helper.js
+++ src/mooks/apis/helper.js
@@ -1,13 +1,13 @@
 import { TIME_OUT_FETCH } from './constant';
 
-const fakePromise = (data, isError = false) => new Promise((resolve, reject) => {
+const fakePromise = (data) => new Promise((resolve, reject) => {
   setTimeout(() => {
-    if (isError) {
+    if (!TIME_OUT_FETCH.status) {
       reject(new Error('Fetch data error !'));
     } else {
       resolve(data);
     }
-  }, TIME_OUT_FETCH);
+  }, TIME_OUT_FETCH.time);
 });
 
 export { fakePromise };
